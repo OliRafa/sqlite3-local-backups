a169dd58960b932040af7db2d1993bcf881c869d
Rafael Augusto de Oliveira <olirafa@protonmail.com>
2024-09-19 23:59:37 +0000

    feat(create-backup): add folder awareness and naive hard links

--- src/sqlite_local_backups/create_backup.py
+++ src/sqlite_local_backups/create_backup.py
@@ -12,11 +12,29 @@ import click
 )
 def create_backup(input_path: Path, output_dir: Path):
     """Create a backup for a given database path."""
-    backup_file_name = (
-        f"{input_path.stem}-{datetime.now().strftime("%Y-%m-%dT%H-%M-%S")}.db"
-    )
+    last_dir = output_dir.joinpath("last")
+    daily_dir = output_dir.joinpath("daily")
+    weekly_dir = output_dir.joinpath("weekly")
+    monthly_dir = output_dir.joinpath("monthly")
+
+    backup_time = datetime.now()
+
+    last_file_name = f"{input_path.stem}-{backup_time.strftime("%Y-%m-%dT%H-%M-%S")}.db"
+    daily_file_name = f"{input_path.stem}-{backup_time.strftime("%Y-%m-%d")}.db"
+    weekly_file_name = f"{input_path.stem}-{backup_time.strftime("%G-%V")}.db"
+    monthly_file_name = f"{input_path.stem}-{backup_time.strftime("%Y-%m")}.db"
+
+    last_file_path = last_dir.joinpath(last_file_name)
+    daily_file_path = daily_dir.joinpath(daily_file_name)
+    weekly_file_path = weekly_dir.joinpath(weekly_file_name)
+    monthly_file_path = monthly_dir.joinpath(monthly_file_name)
+
     run(
-        ["sqlite3", input_path, f"vacuum into '{output_dir}/{backup_file_name}'"],
+        ["sqlite3", input_path, f"vacuum into '{last_file_path}'"],
         encoding="utf8",
         text=True,
     )
+
+    daily_file_path.hardlink_to(last_file_path)
+    weekly_file_path.hardlink_to(last_file_path)
+    monthly_file_path.hardlink_to(last_file_path)
