a4218a2699785b0add2f339a84aa43b508c42a8b
Rafael Augusto de Oliveira <olirafa@protonmail.com>
2024-09-20 00:13:00 +0000

    fix(create-backup): fix hard links not being replaced correctly due to a file already existing error

--- src/sqlite_local_backups/create_backup.py
+++ src/sqlite_local_backups/create_backup.py
@@ -5,10 +5,19 @@ from subprocess import run
 import click
 
 
+def _create_hard_link(file_path: Path, hardlink_path: Path) -> None:
+    try:
+        hardlink_path.hardlink_to(file_path)
+    except FileExistsError:
+        hardlink_path.unlink()
+        hardlink_path.hardlink_to(file_path)
+
+
 @click.command()
 @click.argument("input_path", type=click.Path(exists=True, path_type=Path))
 @click.argument(
-    "output_dir", type=click.Path(exists=True, file_okay=False, path_type=Path)
+    "output_dir",
+    type=click.Path(exists=True, file_okay=False, resolve_path=True, path_type=Path),
 )
 def create_backup(input_path: Path, output_dir: Path):
     """Create a backup for a given database path."""
@@ -29,12 +38,18 @@ def create_backup(input_path: Path, output_dir: Path):
     weekly_file_path = weekly_dir.joinpath(weekly_file_name)
     monthly_file_path = monthly_dir.joinpath(monthly_file_name)
 
+    click.echo(f"Creating backup of {input_path.name}...")
     run(
         ["sqlite3", input_path, f"vacuum into '{last_file_path}'"],
         encoding="utf8",
         text=True,
     )
 
-    daily_file_path.hardlink_to(last_file_path)
-    weekly_file_path.hardlink_to(last_file_path)
-    monthly_file_path.hardlink_to(last_file_path)
+    click.echo(f"Replacing daily backup {daily_file_path} file this last backup...")
+    _create_hard_link(last_file_path, daily_file_path)
+
+    click.echo(f"Replacing weekly backup {weekly_file_path} file this last backup...")
+    _create_hard_link(last_file_path, weekly_file_path)
+
+    click.echo(f"Replacing monthly backup {monthly_file_path} file this last backup...")
+    _create_hard_link(last_file_path, monthly_file_path)
